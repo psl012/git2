f53499e93ae92681a6ecc4fac0315a5e6aedc3c7
v88-paul-samuel <placap@village88.com>
2024-05-28 21:52:06 +0800

    ttes

---
+++ test.py
@@ -0,0 +1,48 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]["last_name"] = "Bryan"
+print(x)
+print(students)
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(students):
+    for element in students:
+        print(f"first_name - {element['first_name']}, last_name - {element['last_name']}")
+
+iterateDictionary(students)
+
+def iterateDictionary2(key_name, some_list):
+    for element in students:
+        print(element[key_name])
+
+iterateDictionary2("last_name", students)
+
+print("================")
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key in some_dict:
+        print(f"{len(some_dict[key])} {key}")
+        for element in some_dict[key]:
+            print(element)
+        print( )
+printInfo(dojo)
